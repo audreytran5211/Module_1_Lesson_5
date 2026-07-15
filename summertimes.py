efeee05e600d4aa7740ada31077a6f89c3813074
audreytran5211 <m5zf8s74t7@privaterelay.appleid.com>
2026-07-14 21:08:14 -0400

    inital commit

---
+++ summertimes.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter the temperature (°C): "))
+
+if temperature >= 25:
+    print("It's warm outside.")
+    print("You can wear light and soft clothes.")
+else:
+    print("It's cold outside.")
+    print("Wear a jacket and a pullover to stay warm.")
