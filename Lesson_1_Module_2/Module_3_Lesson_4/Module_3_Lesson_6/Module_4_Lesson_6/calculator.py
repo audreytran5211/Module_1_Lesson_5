1030b787d8982d5565feafadf30f0dd85f296b0a
audreytran5211 <m5zf8s74t7@privaterelay.appleid.com>
2026-08-29 21:44:37 -0400

    inital commit

---
+++ Lesson_1_Module_2/Module_3_Lesson_4/Module_3_Lesson_6/Module_4_Lesson_6/calculator.py
@@ -0,0 +1,35 @@
+def add(P, Q):
+    # def is always added before a function
+    return P + Q
+def subtract(P, Q):
+    return P - Q
+def multiply(P, Q):
+    return P * Q
+def divide(P, Q):
+    return P / Q
+
+print("Please select an operation:")
+print("a. add")
+print("b. subtract")
+print("c. multiply")
+print("d. divide")
+
+choice = input("enter a choice (a/b/c/d): ")
+#choice is the user input for the operation
+num_1 = int(input("enter first number: "))
+num_2 = int(input("enter second number: "))
+
+if choice == 'a':
+    print(num_1, "+", num_2, "=", add(num_1, num_2))
+
+elif choice == 'b':
+    print(num_1, "-", num_2, "=", subtract(num_1, num_2))
+
+elif choice == 'c':
+    print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+
+elif choice == 'd':
+    print(num_1, "/", num_2, "=", divide(num_1, num_2))
+
+else:
+    print("invalid input")
---
+++ Lesson_1_Module_2/Module_3_Lesson_4/Module_3_Lesson_6/Module_4_Lesson_6/well.wishes.py
@@ -0,0 +1,11 @@
+def well_wishes():
+    print("hellooo")
+    print("have an amazing day!")
+
+well_wishes()
+
+def goodnight():
+    print("good night")
+    print("dont let the bed bugs bite.")
+
+goodnight()
